a888308f4c783a9470f28551e71ffd7513ce27e6
Augusto Aleman <augustoae09@gmail.com>
2024-08-22 18:16:51 -0600

    Added basic infraestructure for connection to frontend and DB components

---
+++ app.py
@@ -0,0 +1,99 @@
+#Import necessary dependencies
+# For server infraestructure
+from flask import Flask, request, jsonify
+import mysql.connector
+from flask_cors import CORS
+
+# For automating emails
+import smtplib 
+from email_response.auto_response import autoResponse
+from email_response.auto_notification import autoNotification
+
+from email_response.auto_response import autoResponse
+from email_response.auto_notification import autoNotification
+
+# Other
+from dotenv import load_dotenv
+from datetime import datetime
+import os
+
+# Loading enviroment variables
+load_dotenv()
+
+# STarting Flask app
+app = Flask(__name__)
+CORS(app)  
+
+# For Db connection to MySQL
+db = mysql.connector.connect(
+    host="localhost",
+    user=os.getenv("DATABASE_USER"),
+    password=os.getenv("DATABASE_PASSWORD"),
+    database=os.getenv("DATABASE")
+)
+
+# Email login credentials
+MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+password_key = os.getenv('MAIL_PASSWORD')
+
+# SMTP Server and port no for GMAIL.com
+gmail_server= "smtp.gmail.com"
+gmail_port= 587
+
+# Starting email server connection
+my_server = smtplib.SMTP(gmail_server, gmail_port)
+my_server.ehlo()
+my_server.starttls()
+
+# Login
+my_server.login(MAIL_USERNAME, password_key)
+
+# /submit -> Receives form data from Frontend formulaire
+@app.route('/submit', methods=['POST'])
+def submit_form():
+    try:
+        # Parse JSON data from request
+        data = request.get_json()
+        if not data:
+            return jsonify({"error": "Invalid JSON data"}), 400
+
+        name = data.get('name')
+        surname = data.get('surname')
+        email = data.get('email')
+        phone = data.get('phone')
+        message = data.get('message')
+
+        # Current timestamp
+        now = datetime.now()
+
+        # Insert data into the MySQL database
+        cursor = db.cursor()
+        sql = "INSERT INTO forms (name, surname, email, phone, message, sent_date) VALUES (%s, %s, %s, %s, %s, %s)"
+        values = (name, surname, email, phone, message, now)
+
+        cursor.execute(sql, values)
+        db.commit()
+    
+        # Send automated response
+        my_server.sendmail(
+                from_addr = MAIL_USERNAME,
+                to_addrs = email,
+                msg = autoResponse(name, email, MAIL_USERNAME)
+            )
+        
+        # Send automated notification
+        my_server.sendmail(
+                from_addr = MAIL_USERNAME,
+                to_addrs = MAIL_USERNAME,
+                msg = autoNotification(name, surname, email, phone, now.strftime("%d-%m-%Y %H:%M:%S"), message,MAIL_USERNAME)
+            )
+
+        return jsonify({"message": "Data submitted successfully"}), 201
+    
+
+    except Exception as e:
+        print(f"Error: {e}")
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ test.py
@@ -0,0 +1,2 @@
+from email_response.auto_response import autoResponse
+print(autoResponse("John Doe", "john@example.com", "sender@example.com"))
